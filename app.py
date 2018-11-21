ddc7ae86b4b0e63c3e9701477b274c27059898fc
Matej Feder <feder.mato@gmail.com>
2018-11-21 22:21:42 +0100

    wip

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('under_construction.html')
+
 
 if __name__ == '__main__':
     app.run()
